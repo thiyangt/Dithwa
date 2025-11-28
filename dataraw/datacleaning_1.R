f7643fc0e7ad29eb8a1523c28f1b82587796e39f
thiyangt <tstalagala@gmail.com>
2025-11-28 15:37:51 +0530

    added files

---
+++ dataraw/datacleaning_1.R
@@ -0,0 +1,46 @@
+## set library
+library(tidyverse)
+library(here)
+library(readxl)
+library(lubridate)
+
+## set the location
+## 8.30 28-11-2025
+Book1 <- read_excel("dataraw/Book1.xlsx")
+View(Book1)
+colnames(Book1)
+Book1$Report_Time <- as.POSIXct(Book1$Report_Time, format = "%Y-%m-%d %H%M")
+Book1
+dithwa_3hr_weather_data <- Book1
+dim(Book1)
+## 11.30 28-11-2025
+library(readxl)
+Book2_11_30_28_11 <- read_excel("dataraw/Book2_11.30_28_11.xlsx")
+View(Book2_11_30_28_11)
+colnames(Book2_11_30_28_11) <- colnames(Book1)
+Book2_11_30_28_11$Report_Time <- as.POSIXct(Book2_11_30_28_11$Report_Time, format = "%Y-%m-%d %H%M")
+colnames(Book2_11_30_28_11)
+Book2_11_30_28_11$Rainfall_mm <- as.numeric(Book2_11_30_28_11$Rainfall_mm)
+dim(Book2_11_30_28_11)
+
+## 2.30 28-11-2025
+library(readxl)
+Book3_2.30PM_28_11_2025 <- read_excel("dataraw/Book3_2.30PM_28_11_2025.xlsx")
+View(Book3_2.30PM_28_11_2025)
+colnames(Book3_2.30PM_28_11_2025) <- colnames(Book1)
+Book3_2.30PM_28_11_2025$Report_Time <- as.POSIXct(Book3_2.30PM_28_11_2025$Report_Time, format = "%Y-%m-%d %H%M")
+colnames(Book3_2.30PM_28_11_2025)
+Book3_2.30PM_28_11_2025$Rainfall_mm <- as.numeric(Book3_2.30PM_28_11_2025$Rainfall_mm)
+dim(Book3_2.30PM_28_11_2025)
+
+## Binding
+dithwa_3hr_weather_data <- bind_rows(dithwa_3hr_weather_data,
+                                     Book2_11_30_28_11)
+dithwa_3hr_weather_data <- bind_rows(dithwa_3hr_weather_data,
+                                     Book3_2.30PM_28_11_2025)
+
+View(dithwa_3hr_weather_data)
+
+dithwa_3hr_weather_data$report <- c(rep(1, 24), rep(2, 24), rep(3, 24))
+View(dithwa_3hr_weather_data)
+usethis::use_data(dithwa_3hr_weather_data)
